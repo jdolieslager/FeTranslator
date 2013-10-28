8b9e8af7a8009a382646ed4fe578c065cc759926
bboer <bartdeboer86@gmail.com>
2013-10-28 17:07:04 +0100

    Becuase zf2 changes classes and interfaces public and protected stuff in minor relases here is a hotfix

--- src/FeTranslator/View/Helper/Translate.php
+++ src/FeTranslator/View/Helper/Translate.php
@@ -2,14 +2,22 @@
 namespace FeTranslator\View\Helper;
 
 use Zend\I18n\Translator\Translator;
+use Zend\ServiceManager\ServiceLocatorAwareInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  * @category    FeTranslator
  * @package     View
  * @subpackage  Helper
  */
-class Translate extends \Zend\I18n\View\Helper\AbstractTranslatorHelper
+class Translate extends \Zend\I18n\View\Helper\AbstractTranslatorHelper implements
+    ServiceLocatorAwareInterface
 {
+    /**
+     * @var ServiceLocatorInterface
+     */
+    protected $serviceLocator;
+
     /**
      * Constructor
      *
@@ -18,6 +26,15 @@ class Translate extends \Zend\I18n\View\Helper\AbstractTranslatorHelper
     public function __construct(Translator $translator)
     {
         $this->setTranslator($translator);
+    }
+
+    /**
+     * Get the translator
+     * @return Translator
+     */
+    public function getTranslator()
+    {
+        return $this->getServiceLocator()->getServiceLocator()->get('FeTranslator\Translator');
     }
 
     /**
@@ -32,5 +49,26 @@ class Translate extends \Zend\I18n\View\Helper\AbstractTranslatorHelper
     {
         $namespace = $namespace ?: $this->getTranslatorTextDomain();
         return $this->getTranslator()->translate($message, $namespace, $locale);
+    }
+
+    /**
+     * Get the service locator
+     * @return ServiceLocatorInterface
+     */
+    public function getServiceLocator()
+    {
+        return $this->serviceLocator;
+    }
+
+    /**
+     * Set the service locator
+     * @param  ServiceLocatorInterface $serviceLocator
+     * @return Translate
+     */
+    public function setServiceLocator(
+        ServiceLocatorInterface $serviceLocator
+    ) {
+        $this->serviceLocator = $serviceLocator;
+        return $this;
     }
 }
